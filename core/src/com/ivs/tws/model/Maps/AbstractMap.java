899007b39d6d2cbd44ff5972f1a27d00c3bfa5cb
MACTEP-ABATAP <61243966+MACTEP-ABATAP@users.noreply.github.com>
2021-04-20 20:01:21 +0300

    Lets repair map

--- core/src/com/ivs/tws/model/Maps/AbstractMap.java
+++ core/src/com/ivs/tws/model/Maps/AbstractMap.java
@@ -1,79 +1,66 @@
 package com.ivs.tws.model.Maps;
 
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteCache;
 import com.badlogic.gdx.maps.Map;
-import com.badlogic.gdx.maps.MapLayers;
-import com.badlogic.gdx.maps.MapProperties;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.utils.TimeUtils;
 
 
 import java.util.Random;
 
 import static com.sun.corba.se.impl.util.RepositoryId.cache;
 
-public class AbstractMap extends Map {
-    public static int WIDTH ;
-    public static int HEIGHT;
-    public static int LAYERS;
-    public static final int TILES_PER_LAYER = WIDTH * HEIGHT;
-    public static final int TILE_WIDTH = 64;
-    public static final int TILE_HEIGHT = 64;
+public class AbstractMap extends TiledMap {
+    private final int LAYERS = 1;
+    public int WIDTH;
+    public int HEIGHT;
+    public int TILES_PER_LAYER = WIDTH * HEIGHT;
+    static final int TILE_WIDTH = 32;
+    static final int TILE_HEIGHT = 32;
+    static final int TILE_HEIGHT_DIAMOND = 32;
+    public final int BOUND_X = HEIGHT * TILE_WIDTH / 2 + WIDTH * TILE_WIDTH / 2;
+    public int BOUND_Y = HEIGHT * TILE_HEIGHT_DIAMOND / 2 + WIDTH * TILE_HEIGHT_DIAMOND / 2;
 
-
-
-    public static final int TILE_HEIGHT_DIAMOND = 32;
-    private int BOUND_X = HEIGHT * TILE_WIDTH / 2 + WIDTH * TILE_WIDTH / 2;
-    private int BOUND_Y = HEIGHT * TILE_HEIGHT_DIAMOND / 2 + WIDTH * TILE_HEIGHT_DIAMOND / 2;
-
-    SpriteCache[] caches = new SpriteCache[LAYERS];
 
     int[] layers = new int[LAYERS];
 
+    long startTime = TimeUtils.nanoTime();
+    public Map Map;
 
 
-    public void generateMap(){
+    public Map create () {
+
+
+        Map = new Map();
+
+
         Random rand = new Random();
         for (int i = 0; i < LAYERS; i++) {
-            caches[i] = new SpriteCache();
-            SpriteCache cache = caches[i];
-            cache.beginCache();
+
+
 
 
             int colX = HEIGHT * TILE_WIDTH / 2 - TILE_WIDTH / 2;
             int colY = BOUND_Y - TILE_HEIGHT_DIAMOND;
-            for (int x = 0; x < WIDTH; x++) { // у или х ?
+            for (int x = 0; x < WIDTH; x++) {
 
 
                 for (int y = 0; y < HEIGHT; y++) {
                     int tileX = colX - y * TILE_WIDTH / 2;
                     int tileY = colY - y * TILE_HEIGHT_DIAMOND / 2;
-                    cache.add(texture, tileX, tileY, rand.nextInt(2) * 64, // что тут?
-                              TILE_WIDTH, TILE_HEIGHT);
                 }
                 colX += TILE_WIDTH / 2;
                 colY -= TILE_HEIGHT_DIAMOND / 2;
             }
-            layers[i] = cache.endCache();
         }
+        return Map;
+    }
+
+
+    public void disposeMap () {
+        Map.dispose();
     }
 
 
 
-
-
-    private MapProperties properties = new MapProperties();
-
-    @Override
-    public MapProperties getProperties() {
-        return properties;
-    }
-
-
-
-
-    @Override
-    public void dispose() {
-        super.dispose();
-    }
 }
